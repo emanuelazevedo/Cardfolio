abcac3ffdf5f0d78fe300f02e3ef1204940c6ecb
Emanuel <emanuel.azevedo@ua.pt>
2020-02-23 22:18:24 +0000

    change of pic on server-test

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/manu/Desktop/Cardfolio/.cache/dev-404-page.js"))),
   "component---src-pages-index-tsx": hot(preferDefault(require("/home/manu/Desktop/Cardfolio/src/pages/index.tsx")))
 }
 
